0a2dfb2dceccac3e445fd7ae9ed19f9efa7c3f0f
Roy Konstantin <royks@skbkontur.ru>
2018-04-07 14:32:08 +0500

    player move

---
+++ ff/Assets/scripts/AutosaveScene.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutosaveScene
+{
+    static AutosaveScene()
+    {
+        EditorApplication.playmodeStateChanged = () =>
+        {
+
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                EditorSceneManager.SaveOpenScenes();
+            }
+        };
+    }
+}
+
+#endif
---
+++ ff/Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace Assets.scripts
+{
+    [SuppressMessage("ReSharper", "UnassignedField.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public class PlayerController : MonoBehaviour
+    {
+        public float PlayerSpeed;
+        private Transform _trans;
+        private float _moveX = 0f;
+        private float _moveY = 0f;
+
+        private void Awake()
+        {
+            _trans = transform;
+        }
+
+        private void Update()
+        {
+            _moveX = Input.GetAxis("Horizontal") * PlayerSpeed;
+            _moveY = Input.GetAxis("Vertical") * PlayerSpeed;
+        }
+
+        private void FixedUpdate()
+        {
+            _trans.position = _trans.position.AddValueXY(_moveX, _moveY);
+        }
+    }
+}
