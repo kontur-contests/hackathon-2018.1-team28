c3e191a201ea12ca0359f430d7ab65f316fa7595
Roy Konstantin <royks@skbkontur.ru>
2018-04-07 21:36:23 +0500

    bug enemy prefab

---
+++ ff/Assets/scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.scripts;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public int Damage;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var damagedObject = col.gameObject.GetComponent<IDamageable>();
+        damagedObject?.GetDamage(Damage);
+    }
+}
