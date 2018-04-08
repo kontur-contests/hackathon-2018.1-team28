d19a002e44fe46054154824c83e295f21021907c
Roy Konstantin <royks@skbkontur.ru>
2018-04-08 11:04:13 +0500

    Added Enemy500

--- ff/Assets/scripts/EnemyController.cs
+++ ff/Assets/scripts/EnemyController.cs
@@ -11,83 +11,91 @@ namespace Assets.scripts
     [SuppressMessage("ReSharper", "UnusedMember.Local")]
     public class EnemyController : MonoBehaviour, IDamageable
     {
-        private Transform _trans;
-        private EnemyData _data;
         private AudioSource _audio;
-        private bool IsAlive => _data.IsAlive;
-		private GameObject _player;
+        private EnemyData _data;
+        private GameObject _player;
         private SpawnerController _spawner;
-		private float _moveX;
-		private float _moveY;
-
-        private void Awake()
-        {
-            _data = GetComponent<EnemyData>();
-			_player = GameObject.FindGameObjectWithTag("Player");
-            _spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<SpawnerController>();
-            _audio = GetComponent<AudioSource>();
-            _audio.clip = _data.MoveSound;
-            _audio.Play();
-        }
-
-		private void Update()
-		{
-
-		}
-
-        private void FixedUpdate()
-        {			
-			if (!CheckAlive())
-				return;
-			
-			Rotate ();
-			Move();
-        }
-
-        private bool CheckAlive() => IsAlive;
-
-		private void Move()
-		{			
-			var step = _data.Speed * Time.deltaTime;
-			transform.position = Vector3.MoveTowards (transform.position, _player.transform.position.ChangeZ(transform.position.z), step);
-		}
-
-		private void Rotate()
-		{
-			var angle = MathHelper.AngleBetweenTwoPoints(_player.transform.position, transform.position);
-			transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-		}
-
-        private void OnTriggerEnter2D(Collider2D col)
-        {
-			if (IsAlive)
-			{
-				var damagedObject = col.gameObject.GetComponent<IDamageable> ();
-			    if (damagedObject != null && !col.gameObject.CompareTag("Enemy"))
-			    {
-			        damagedObject.GetDamage(_data.Damage);
-                }
-			}
-        }
+        private bool IsAlive => _data.IsAlive;
 
         public void GetDamage(int damage)
         {
             _data.HealthPoint -= damage;
-            Debug.Log(_data.HealthPoint);
+
             if (!IsAlive)
-            {
                 Die();
-            }                
         }
 
         public void Die()
         {
-            Debug.Log("Dead bug");
-            _audio.clip = _data.DieSound;
-            _audio.loop = false;
-            _audio.Play();
+            if (_data.DieSound != null)
+            {
+                _audio.clip = _data.DieSound;
+                _audio.loop = false;
+                _audio.Play();
+            }
+
             Destroy(gameObject, 1);
-            _spawner.Decrement();
+            if (_spawner != null)
+            {
+                _spawner.Decrement();
+            }
+
+        }
+
+        private void Awake()
+        {
+            _data = GetComponent<EnemyData>();
+            _player = GameObject.FindGameObjectWithTag("Player");
+            //_spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<SpawnerController>();
+            _audio = GetComponent<AudioSource>();
+            if (_data.MoveSound != null)
+            {
+                _audio.clip = _data.MoveSound;
+                _audio.Play();
+            }
+        }
+
+        public void SetSpawner(SpawnerController spawner)
+        {
+            _spawner = spawner;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!CheckAlive())
+                return;
+
+            if (_data.EnableRotate)
+                Rotate();
+
+            Move();
+        }
+
+        private bool CheckAlive()
+        {
+            return IsAlive;
+        }
+
+        private void Move()
+        {
+            var step = _data.Speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position,
+                _player.transform.position.ChangeZ(transform.position.z), step);
+        }
+
+        private void Rotate()
+        {
+            var angle = MathHelper.AngleBetweenTwoPoints(_player.transform.position, transform.position);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!IsAlive) return;
+
+            var damagedObject = col.gameObject.GetComponent<IDamageable>();
+            if (damagedObject != null && !col.gameObject.CompareTag("Enemy"))
+                damagedObject.GetDamage(_data.Damage);
         }
     }
 }
--- ff/Assets/scripts/SpawnerController.cs
+++ ff/Assets/scripts/SpawnerController.cs
@@ -1,51 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using UnityEngine;
 
-public class SpawnerController : MonoBehaviour 
+namespace Assets.scripts
 {
-	public Transform EnemyPrefab;
-	public int MaxCount;
-	public float SpawnDistance;
-
-	private GameObject _player;
-	private int _count;
-
-    public void Decrement()
+    [SuppressMessage("ReSharper", "UnassignedField.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    public class SpawnerController : MonoBehaviour
     {
-        if(_count > 0)
-            Interlocked.Decrement(ref _count);
+        private int _count;
+
+        private GameObject _player;
+        public Transform EnemyPrefab;
+        public int MaxCount;
+        public float SpawnDistance;
+
+        public void Decrement()
+        {
+            if (_count > 0)
+                Interlocked.Decrement(ref _count);
+        }
+
+        private void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _count = 0;
+        }
+
+        private void Update()
+        {
+            if (_count < MaxCount && Random.Range(0, 10) % 2 == 0)
+            {
+                Interlocked.Increment(ref _count);
+                Spawn();
+                Debug.Log($"Spawned bug {_count} of {MaxCount}");
+            }
+        }
+
+        private void Spawn()
+        {
+            var enemy = Instantiate(EnemyPrefab, GetPointToSpawn(), Quaternion.identity);
+            enemy.GetComponent<EnemyController>().SetSpawner(this);
+        }
+
+        private Vector3 GetPointToSpawn()
+        {
+            var radian = Random.Range(0f, Mathf.PI * 2);
+            var x = Mathf.Cos(radian);
+            var y = Mathf.Sin(radian);
+            var spawnPoint = new Vector3(x, y, 0) * SpawnDistance;
+            return _player.transform.position + spawnPoint;
+        }
     }
-
-    private void Start () 
-	{
-		_player = GameObject.FindGameObjectWithTag ("Player");
-		_count = 0;
-	}
-
-	private void Update () 
-	{
-	    if (_count < MaxCount && Random.Range(0, 10) % 2 == 0)
-	    {
-	        Interlocked.Increment(ref _count);
-            Spawn();
-	        Debug.Log($"Spawned bug {_count} of {MaxCount}");
-        }			
-	}
-
-	private void Spawn()
-	{	    
-        Instantiate(EnemyPrefab, GetPointToSpawn(), Quaternion.identity);	    
-	}
-
-	private Vector3 GetPointToSpawn()
-	{
-		float radian = Random.Range(0f, Mathf.PI*2);
-		var x = Mathf.Cos(radian);
-		var y = Mathf.Sin(radian);
-		var spawnPoint = new Vector3 (x, y, 0) * SpawnDistance;
-		return _player.transform.position + spawnPoint;
-	}
-	
 }
