3afe183e2ce217b1532ef68a277355137e441e6c
mplyusnin <mplyusnin@skbkontur.ru>
2018-04-08 11:36:13 +0500

    Add game state obj

--- ff/Assets/scripts/GameMenu/LoadGameScene.cs
+++ ff/Assets/scripts/GameMenu/LoadGameScene.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,8 @@ public class LoadGameScene : MonoBehaviour
 
     public void LoadMainScene(int difficult)
     {
+        GlobalGameState.ChangeDifficult(difficult);
+
         SceneManager.LoadScene("OpenWorld", LoadSceneMode.Single);
     }
 
---
+++ ff/Assets/scripts/GlobalGameState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.scripts
+{
+    public class GlobalGameState
+    {
+        private static readonly GlobalGameState instance = new GlobalGameState();
+
+        public int Difficult { get; private set; }
+
+        private GlobalGameState()
+        {
+            Difficult = 1;
+        }
+
+        public static void ChangeDifficult(int difficult)
+        {
+            instance.Difficult = difficult;
+        }
+
+        public static GlobalGameState GetInstance()
+        {
+            return instance;
+        }
+    }
+}
