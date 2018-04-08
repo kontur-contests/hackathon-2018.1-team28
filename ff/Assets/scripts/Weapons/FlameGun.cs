d24a8e8b1def09552f5269698eefb4004e24ef2e
Roy Konstantin <royks@skbkontur.ru>
2018-04-08 08:17:41 +0500

    added flamegun (but sounds like machinegun)

---
+++ ff/Assets/scripts/Weapons/FlameGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.scripts.Weapons
+{
+    [PublicAPI]
+    [SuppressMessage("ReSharper", "UnassignedField.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public class FlameGun : MonoBehaviour, IWeapon
+    {
+
+        public GameObject Bullet;
+        public float FireDelay;
+
+        public float GetFireDelay() => FireDelay;
+
+        public GameObject GetBullet() => Bullet;
+
+        public void Shoot(Vector3 startPosition, Vector3 shootAngle)
+        {
+            var bullet = Instantiate(Bullet);
+            var bulletController = bullet.GetComponent<BulletController>();
+            bulletController.transform.position = startPosition;
+            bulletController.ShootAngle = shootAngle;
+        }
+    }
+}
