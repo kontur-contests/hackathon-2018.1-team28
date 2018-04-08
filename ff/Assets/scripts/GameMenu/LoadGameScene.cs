3834c2143042bdc934b4ada01b590053b08e888d
mplyusnin <mplyusnin@skbkontur.ru>
2018-04-08 09:34:22 +0500

    update mainmenu

---
+++ ff/Assets/scripts/GameMenu/LoadGameScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour
+{
+
+    public void LoadMainScene(int difficult)
+    {
+        SceneManager.LoadScene("OpenWorld", LoadSceneMode.Single);
+    }
+
+}
