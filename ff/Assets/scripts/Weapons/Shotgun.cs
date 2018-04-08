63e3dbcd4dee628360ef9dbbb8ef82be8a628f3a
Roy Konstantin <royks@skbkontur.ru>
2018-04-08 07:54:59 +0500

    added shotgun

--- ff/Assets/scripts/Weapons/Shotgun.cs
+++ ff/Assets/scripts/Weapons/Shotgun.cs
@@ -7,16 +7,26 @@ namespace Assets.scripts.Weapons
     [PublicAPI]
     [SuppressMessage("ReSharper", "UnassignedField.Global")]
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-    public class Pistol : MonoBehaviour, IWeapon
+    public class Shotgun : MonoBehaviour, IWeapon
     {
         public GameObject Bullet;
         public float FireDelay;
+        public float AngleDelta;
 
-        public float GetFireDelay()=>FireDelay;
+        public float GetFireDelay() => FireDelay;
 
         public GameObject GetBullet() => Bullet;
 
         public void Shoot(Vector3 startPosition, Vector3 shootAngle)
+        {
+            ShootOne(startPosition, shootAngle + new Vector3(AngleDelta, 0));
+            ShootOne(startPosition, shootAngle + new Vector3(-AngleDelta, 0));
+            ShootOne(startPosition, shootAngle);
+            ShootOne(startPosition, shootAngle + new Vector3(0, AngleDelta));
+            ShootOne(startPosition, shootAngle + new Vector3(0, -AngleDelta));
+        }
+
+        private void ShootOne(Vector3 startPosition, Vector3 shootAngle)
         {
             var bullet = Instantiate(Bullet);
             var bulletController = bullet.GetComponent<BulletController>();
